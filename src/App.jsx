3575b8bf29e12317a42972191e5e199a6a9c9a75
ulysses-ck <ulisesapaza.280@gmail.com>
2024-09-22 01:50:05 -0300

    Refactor: Add card flipping logic and matching functionality
    This commit adds the necessary logic to handle card flipping and matching in the Match Pair Game. It introduces the `createSignal` and `createEffect` functions from Solid.js to manage the state of the game. The `handleOnClickCard` function is implemented to handle the click event on each card, allowing the player to flip them. When two cards are flipped, the logic checks if they match based on their content. If they match, the cards are added to the `flipped` array, and if they don't match, the cards are flipped back. This commit also updates the rendering logic to conditionally show the flipped cards and the button to flip the cards.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,62 @@
+import { createEffect, createSignal, For } from "solid-js";
+
 function App() {
+  const [cards, setCards] = createSignal([
+    { id: 1, content: "🍕" },
+    { id: 2, content: "🍕" },
+    { id: 3, content: "🍔" },
+    { id: 4, content: "🍔" },
+    { id: 5, content: "🍟" },
+    { id: 6, content: "🍟" },
+    { id: 7, content: "🍦" },
+    { id: 8, content: "🍦" },
+  ]);
+
+  const [flipped, setFlipped] = createSignal([]);
+
+  const [currentFlipOne, setCurrentFlipOne] = createSignal(null);
+  const [currentFlipTwo, setCurrentFlipTwo] = createSignal(null);
+
+  const handleOnClickCard = (card) => {
+    if (currentFlipOne() === null) {
+      setCurrentFlipOne(card);
+      return;
+    } else if (currentFlipTwo() === null) {
+      setCurrentFlipTwo(card);
+    }
+  };
+
+  createEffect(() => {
+    console.log("flipped", flipped());
+    console.log("currentFlipOne", currentFlipOne());
+    console.log("currentFlipTwo", currentFlipTwo());
+
+    if (currentFlipOne() !== null && currentFlipTwo() !== null) {
+      if (currentFlipOne().content === currentFlipTwo().content) {
+        setFlipped([...flipped(), currentFlipOne(), currentFlipTwo()]);
+      }
+      setCurrentFlipOne(null);
+      setCurrentFlipTwo(null);
+    }
+  });
+
   return (
     <main>
       <h1>Match Pair Game</h1>
+      <For each={cards()}>
+        {(item) => (
+          <Show
+            when={
+              !flipped().includes(item) &&
+              currentFlipOne() !== item &&
+              currentFlipTwo() !== item
+            }
+            fallback={<button>{item.content}</button>}
+          >
+            <button onClick={() => handleOnClickCard(item)}>👀</button>
+          </Show>
+        )}
+      </For>
     </main>
   );
 }
